64813e528478079343e091e3d57dd08b23d5f734
Tenzai <89777119+Gilg4me3h@users.noreply.github.com>
2025-08-24 12:04:11 +0200

    Add files via upload

---
+++ c02/ex10/ft_strlcpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mballout <mballout@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/23 14:00:00 by mballout          #+#    #+#             */
+/*   Updated: 2025/08/23 18:04:47 by mballout         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcpy(char *dest, const char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	while (src[i] != '\0')
+		i++;
+	if (size == 0)
+		return (i);
+	j = 0;
+	while (src[j] != '\0' && j < size - 1)
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	dest[j] = '\0';
+	return (i);
+}
